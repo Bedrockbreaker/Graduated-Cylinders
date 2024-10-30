c91933563430704655a14bd10e83914427e8e6b4
Bedrockbreaker <coolmandude101@gmail.com>
2024-10-30 00:05:12 -0700

    Use injected version

--- src/main/java/bedrockbreaker/graduatedcylinders/GraduatedCylinders.java
+++ src/main/java/bedrockbreaker/graduatedcylinders/GraduatedCylinders.java
@@ -14,11 +14,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = GraduatedCylinders.MODID, name = GraduatedCylinders.NAME, version = GraduatedCylinders.VERSION)
+@Mod(modid = GraduatedCylinders.MODID, name = GraduatedCylinders.NAME, version = Tags.VERSION)
 public class GraduatedCylinders {
 	public static final String MODID = "graduatedcylinders";
 	public static final String NAME = "Graduated Cylinders";
-	public static final String VERSION = "3.1.0";
 
 	public static Logger console = LogManager.getLogger("Graduated Cylinders");
 	public static boolean isMekanismLoaded = false;
