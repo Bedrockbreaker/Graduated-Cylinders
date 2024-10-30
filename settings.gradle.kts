35bc8bcd8bdf6647a4a389668f4264c9c9e657eb
Bedrockbreaker <coolmandude101@gmail.com>
2024-10-29 23:47:45 -0700

    Formatting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,23 @@
-rootProject.name = "RFGExampleMod"
+rootProject.name = "GraduatedCylinders"
 
 pluginManagement {
-  repositories {
-    maven {
-      // RetroFuturaGradle
-      name = "GTNH Maven"
-      url = uri("https://nexus.gtnewhorizons.com/repository/public/")
-      mavenContent {
-        includeGroupByRegex("com\\.gtnewhorizons\\..+")
-        includeGroup("com.gtnewhorizons")
-      }
-    }
-    gradlePluginPortal()
-    mavenCentral()
-    mavenLocal()
-  }
+	repositories {
+		maven {
+			// RetroFuturaGradle
+			name = "GTNH Maven"
+			url = uri("https://nexus.gtnewhorizons.com/repository/public/")
+			mavenContent {
+				includeGroupByRegex("com\\.gtnewhorizons\\..+")
+				includeGroup("com.gtnewhorizons")
+			}
+		}
+		gradlePluginPortal()
+		mavenCentral()
+		mavenLocal()
+	}
 }
 
 plugins {
-  // Automatic toolchain provisioning
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+	// Automatic toolchain provisioning
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
